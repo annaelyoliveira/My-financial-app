fd7565005745abf18e9ffee99e000d1af992037c
Annaely Oliveira <annaely691@gmail.com>
2026-06-30 17:57:55 -0300

    Revisão código for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx'
 import { FinanceProvider } from './context/FinanceContext.jsx'
@@ -10,9 +10,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <FinanceProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </FinanceProvider>
     </ThemeProvider>
   </React.StrictMode>,
